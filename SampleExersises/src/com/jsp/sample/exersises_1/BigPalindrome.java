048f5ea75277f831fdc916eecdaca337c70c4c14
Sidhardh <Choukanavarmadhura6@gmail.com>
2023-02-18 18:43:13 +0530

    Create BigPalindrome.java

---
+++ SampleExersises/src/com/jsp/sample/exersises_1/BigPalindrome.java
@@ -0,0 +1,44 @@
+package com.jsp.sample.exersises_1;
+
+import java.util.Scanner;
+
+public class BigPalindrome {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter the string");
+		String s=scn.nextLine();
+		String bigPal="";
+		for(int i=0;i<s.length()-1;i++)
+		{
+			for(int j=i+1;j<s.length();j++)
+			{
+				if(isPal(s,i,j))
+				{
+					String temp=s.substring(i,j+1);
+					if(temp.length()>=bigPal.length())
+					{
+						bigPal=temp;
+					}
+				}
+				
+			}
+		}
+		System.out.println(bigPal);
+
+	}
+	public static boolean isPal(String s,int start,int end)
+	{
+		char[] a=s.toLowerCase().toCharArray();
+		int i=start;
+		int j=end;
+		 while(i<j)
+		 {
+			 if(a[i]!=a[j])return false;
+			 i++;
+			 j--;
+		 }
+		 return true;
+	}
+
+}
